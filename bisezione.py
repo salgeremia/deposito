5eb509d40d222c0acc0b987e4190e5d0b77b9b44
salgeremia <salgeremia@gmail.com>
2023-10-12 12:47:21 +0200

    add bisezione.py

---
+++ bisezione.py
@@ -0,0 +1,31 @@
+def f(x):
+    return x**3 - 3
+
+def exist(a, b):
+    return True if f(a)*f(b) < 0 else False
+'''
+    if f(a)*f(b) < 0:
+        return True
+    else:
+        return False
+'''
+
+def m(a, b):
+    return (a+b)/2
+
+
+a = -5
+b = 5
+
+while abs(a-b) > 0.001:
+    print(a, b)
+    if exist(a, b):
+        med = m(a, b)
+        if exist(a, med):
+            b = med
+        elif exist(med, b):
+            a = med
+        else:
+            print('BABBIONE hai trovato lo zero ->', med)
+            break
+
