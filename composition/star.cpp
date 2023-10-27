692be5ee5d1a9e9870b909c02dc92165295a00a3
salgeremia <salgeremia@gmail.com>
2023-10-27 21:54:43 +0200

    update star.cpp

--- composition/star.cpp
+++ composition/star.cpp
@@ -1,3 +1,26 @@
+/* https://www.jitbit.com/screensharing/#9026157028323334145
+Realizza la classe Stella, composta da oggetti 
+della classe Pentagono e oggetti della classe Triangolo. 
+Le classi Pentagono e Triangolo sono entrambe figlie 
+della classe Poligono, che possiede la funzione virtuale 
+perimetro. La classe Triangolo possiede due costruttori, 
+il primo ha un solo parametro ed è utilizzato per costruire 
+triangoli equilateri, il secondo ha due parametri 
+(base e lato obliquo) ed è utilizzato per costruire 
+triangoli isosceli. 
+Mostra un utilizzo dell'oggetto "stella" nel main.
+- - - - - - - - - - - - - - - - - - - - - - - - - - 
+ATTRIBUZIONE DEI PUNTEGGI
+
+1. class Poligono = 20 pt
+2. class Pentagono = 15 pt
+3. class Triangolo = 30 pt
+4. class Stella = 25 pt
+5. leggibilità del codice = 10 pt
+- - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
+
 #include <iostream>
 
 using namespace std;
@@ -81,4 +104,3 @@ int main()
     Stella stella = Stella(p, t);
     cout << "Il perimetro della STELLA è " << stella.perimetro() << endl;
 }
-
