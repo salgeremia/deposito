995082a6ec78b42b955ef906c68480ca866cfa6d
salgeremia <salgeremia@gmail.com>
2024-09-20 19:59:48 +0200

    class Rettangolo: add methods and use example

--- oop/rettangolo.py
+++ oop/rettangolo.py
@@ -3,3 +3,15 @@ class Rettangolo:
     def __init__(self, base, altezza):
         self.base = base
         self.altezza = altezza
+
+    def perimetro(self):
+        return 2 * (self.base + self.altezza)
+    
+    def area(self):
+        return self.base * self.altezza
+    
+    def diagonale(self):
+        return (self.base**2 + self.altezza**2)**0.5
+
+r = Rettangolo(4, 3)
+print(r.perimetro(), r.area(), r.diagonale())
