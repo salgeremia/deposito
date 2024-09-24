5257388af141e2252582585134e144d04b901c84
salgeremia <salgeremia@gmail.com>
2024-09-24 10:07:44 +0200

    add @classmethod in sprite.py

--- oop/sprite.py
+++ oop/sprite.py
@@ -10,9 +10,18 @@ class Sprite:
 
     def distance_to(self, other):
         print(f'{self.name} -> {other.name}')
-
+        return Sprite.distance_between(self, other)
+    
+    @classmethod
+    def distance_between(cls, obj_sprite_1, obj_sprite_2):
+        distance_x = obj_sprite_1.x - obj_sprite_2.x
+        distance_y = obj_sprite_1.y - obj_sprite_2.y
+        distance = (distance_x**2 + distance_y**2)**0.5
+        return distance
 
 s1 = Sprite('Dog', 20, 0)
-s2 = Sprite('Cat', 60, 0)
-s1.distance_to(s2)
-print(s1)
+s2 = Sprite('Cat', 60, 30)
+
+d = s1.distance_to(s2)
+print(d)
+
