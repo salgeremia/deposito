fd1c3395a4ff13034352a66a875ef54587d663ec
salgeremia <salgeremia@gmail.com>
2024-10-05 14:16:22 +0200

    add oop/bank_account.py

---
+++ oop/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, balance: float, holder: str) -> None:
+        self.set_balance(balance)
+        self.holder = holder
+
+    def __str__(self) -> str:
+        return f'{self.holder} = {self.get_balance()}'
+    
+    def set_balance(self, balance: float) -> None:
+        self.__balance = balance
+
+    def get_balance(self) -> float:
+        return self.__balance
+    
+    def deposit(self, amount: float) -> None:
+        self.set_balance(self.get_balance() + amount) 
+
+    def withdraw(self, amount: float) -> None:
+        self.set_balance(self.get_balance() - amount) 
+
+    
+
+ba = BankAccount(0, 'Geremia')
+ba.deposit(1048.50)
+ba.withdraw(420)
+money = ba.get_balance()
+print(money)
