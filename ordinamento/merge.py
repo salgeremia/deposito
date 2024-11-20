8fff1f8d5d45de2f2fd37ed51babc6c281534837
salgeremia <salgeremia@gmail.com>
2024-11-20 15:20:20 +0100

    update merge.py

--- ordinamento/merge.py
+++ ordinamento/merge.py
@@ -1,25 +1,34 @@
-def merge(one: list[int], two: list[int]):
+def merge(left: list[int], right: list[int]):
     result = list()
     i, j = 0, 0
-    while i < len(one) and j < len(two):
-        if one[i] < two[j]:
-            result.append(one[i])
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
             i += 1
         else:
-            result.append(two[j])
+            result.append(right[j])
             j += 1
-    
-    if i == len(one):
-        for e in two[j:]:
-            result.append(e)
-    
-    if j == len(two):
-        for e in one[i:]:
+    if i == len(left):
+        for e in right[j:]:
             result.append(e)
 
+    elif j == len(right):
+        for e in left[i:]:
+            result.append(e)
     return result
 
+def merge_sort(v: list[int]):
+    if len(v) > 1:
+        med = len(v) // 2
+        l = v[:med]
+        r = v[med:]
+        print(l, r)
+        sorted_left = merge_sort(l)
+        sorted_right = merge_sort(r)
+        return merge(sorted_left, sorted_right)
+    else:
+        return v
 
-a = [2, 4, 6]
-b = [1, 3, 5]
-print(merge(a, b))
+
+x = [5, 1, 4, 3]
+print(merge_sort(x))
