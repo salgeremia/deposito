31f501aeec29c2cfa69db5b9b3efa7e93e152cfb
salgeremia <salgeremia@gmail.com>
2024-12-02 14:41:52 +0100

    add lista_concatenata.py

---
+++ abstract_data_type/lista_concatenata.py
@@ -0,0 +1,32 @@
+from random import randint
+
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = self.tail = None
+
+    def __str__(self) -> str:
+        text = ''
+        node = self.head
+        while node:
+            text += f'{node.value} -> '     # type: ignore
+            node = node.next                # type: ignore
+        return f'{text} None'
+        
+    def append(self, value) -> None:
+        node = Node(value)
+        if not self.head:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node           # type: ignore
+            self.tail = node
+
+
+l_list = LinkedList()
+for _ in range(randint(2, 10)):
+    l_list.append(randint(1, 100))
+print(l_list)
