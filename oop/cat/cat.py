67d6cc292c0142ab97340813faa9f4773d3255d6
salgeremia <salgeremia@gmail.com>
2025-09-30 16:01:10 +0200

    add change_name method in cat.py

--- oop/cat/cat.py
+++ oop/cat/cat.py
@@ -3,13 +3,17 @@ class Cat:
         self.__name = name
     
     def __str__(self) -> str:
-        return f'Cat name: {self.__name}'
+        return f'> cat name: {self.__name}'
     
-    def mew(self) -> None:
-        print('Meooow')
+    def meow(self) -> None:
+        print('meooow')
+
+    def change_name(self, new_name: str) -> None:
+        self.__name = new_name
 
 
 c = Cat('Sibilla')
-c.mew()
 print(c)
-
+c.meow()                # Sibilla meows...
+c.change_name('Pepe')   # change cat's name from "Sibilla" to "Pepe"
+print(c)
