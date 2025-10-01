4a28dc7dcacb3936b2c9b378c1ec9c05be13e7de
salgeremia <salgeremia@gmail.com>
2025-10-01 08:31:01 +0200

    update cat.py

--- oop/cat/cat.py
+++ oop/cat/cat.py
@@ -17,3 +17,13 @@ print(c)
 c.meow()                # Sibilla meows...
 c.change_name('Pepe')   # change cat's name from "Sibilla" to "Pepe"
 print(c)
+
+# let's try changing the cat's name in another way
+c.__name = 'Trilly'
+print(c)
+# as you can see, it doesn't work!
+
+# even though a (dangerous) way exists:
+c._Cat__name = 'Trilly'
+print(c)
+# now that you know, forget it.
