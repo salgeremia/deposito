5c3b7a366f2d6a6842216798bc26d6d9b83bcde2
salgeremia <salgeremia@gmail.com>
2025-10-16 16:11:13 +0200

    add draft test on if-else statement

---
+++ costrutti_condizionali/calcolatrice.py
@@ -0,0 +1,13 @@
+# Calcolatrice con menù
+'''
+Scrivi un programma che:
+- Chieda all’utente due numeri.
+- Mostri un menù con le operazioni possibili:
+1. Somma
+2. Sottrazione
+3. Moltiplicazione
+4. Divisione
+- Legga la scelta dell’utente e applichi l’operazione corrispondente.
+- Se l’utente sceglie la divisione, verifica che il divisore non sia zero.
+- Se la scelta non è tra 1 e 4, stampa "Scelta non valida".
+'''
---
+++ costrutti_condizionali/if-else_annidati.py
@@ -0,0 +1,23 @@
+'''
+1. Anno bisestile
+Scrivi un programma che determini se un anno è bisestile.
+
+Regola:
+un anno è bisestile se è divisibile per 4, ma non per 100, 
+oppure se è divisibile per 400.
+
+(Suggerimento: usa condizioni annidate.)
+'''
+
+'''
+2. Controllo di accesso con password
+
+Chiedi all’utente di inserire un nome utente.
+Se il nome utente è "admin", chiedi anche la password.
+
+Se la password è "1234", stampa "Accesso consentito".
+Altrimenti stampa "Password errata".
+
+Se il nome utente non è "admin", stampa "Utente non riconosciuto".
+'''
+    
