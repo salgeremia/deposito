c021742833264548df12cd313ef88a28cc03f453
salgeremia <salgeremia@gmail.com>
2025-10-16 11:18:59 +0200

    add if-else.py

---
+++ costrutti_condizionali/if-else.py
@@ -0,0 +1,11 @@
+# https://www.jitbit.com/screensharing/#4482707301447075508
+'''
+1. Numero pari o dispari
+Scrivi un programma che chieda all’utente un numero intero (variabile n) 
+e visualizzi mediante un messaggio a video se è pari o dispari.
+'''
+n = int(input('> inserire un numero intero: '))
+if n%2 == 0:
+    print('-> NUMERO PARI')
+else:
+    print('-> NUMERO DISPARI')
