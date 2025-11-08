f9f3bf06e00ff97c1963175a6621699a8ae7c024
salgeremia <salgeremia@gmail.com>
2025-11-08 13:58:09 +0100

    update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,6 +7,17 @@ URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+
+cards = soup.find_all("div", class_="card-content")
+for card in cards[:1]:
+    title = card.find("h2")
+    subtitle = card.find("h3")
+    location = card.find("p", class_="location")
+    time = card.find("time")
+    print(title, subtitle, location, time, sep='\n', end='\n\n')
+    
+'''
 h2 = soup.find_all('h2')
 for i in range(6):
     print(h2[i].text)
+'''    
