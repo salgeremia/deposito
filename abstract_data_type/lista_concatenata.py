f5fe2782c335869a53bc9b5c1e83816e48d94121
salgeremia <salgeremia@gmail.com>
2025-12-02 16:02:15 +0100

    add insert method to lista_concatenata.py

--- abstract_data_type/lista_concatenata.py
+++ abstract_data_type/lista_concatenata.py
@@ -8,25 +8,52 @@ class Node:
 class LinkedList:
     def __init__(self) -> None:
         self.head = self.tail = None
+        self.len = 0
 
     def __str__(self) -> str:
         text = ''
         node = self.head
         while node:
-            text += f'{node.value} -> '     # type: ignore
-            node = node.next                # type: ignore
-        return f'{text} None'
+            text += f'{node.value} -> '                 # type: ignore
+            node = node.next                            # type: ignore
+        return f'{text}|'
         
     def append(self, value) -> None:
         node = Node(value)
         if not self.head:
             self.head = self.tail = node
         else:
-            self.tail.next = node           # type: ignore
+            self.tail.next = node                       # type: ignore
             self.tail = node
+        self.len += 1
+    
+    def insert(self, index: int, value) -> None:
+        if index < 0 or index > self.len:
+            raise IndexError('Index out of bounds')
+        
+        if index == self.len:
+            self.append(value)
+            return
+        else:
+            node = Node(value)
+            if index == 0:
+                node.next = self.head                   # type: ignore
+                self.head = node
+            else:
+                current_node = self.head
+                for _ in range(index-1):
+                    current_node = current_node.next    # type: ignore
+                node.next = current_node.next           # type: ignore
+                current_node.next = node                # type: ignore
+            self.len += 1 
 
 
 l_list = LinkedList()
 l_list.append(7)
 l_list.append(3)
+l_list.append(5)
+l_list.insert(1, 4)
+l_list.insert(0, 9)
+# l_list.insert(8, 8)
+l_list.insert(5, 8)
 print(l_list)
