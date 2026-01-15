1507044440cdb0f3e4eec652d9f2fd60f852ef53
salgeremia <salgeremia@gmail.com>
2026-01-15 08:51:22 +0100

    add __str__ and __contains__ methods to BST class

--- abstract_data_type/alberi/binary_search_tree.py
+++ abstract_data_type/alberi/binary_search_tree.py
@@ -4,6 +4,22 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
+    def __str__(self) -> str:
+        if self.value is None:
+            return ''
+        else:
+            return f'{self.left} {self.value} {self.right}'
+
+    def __contains__(self, value: int) -> bool:
+        if self.value is None:
+            return False
+        elif self.value == value:
+            return True
+        elif value < self.value:
+            return self.left.__contains__(value)       # type: ignore
+        else:
+            return self.right.__contains__(value)      # type: ignore
+        
     def insert(self, value: int) -> None:
         if self.value is None:
             self.value = value
@@ -13,11 +29,15 @@ class BinarySearchTree:
             if value < self.value:
                 self.left.insert(value)     # type: ignore
             else:
-                self.right.insert(value)    # type: ignore
+                self.right.insert(value)    # type: ignore  
 
 
 tree = BinarySearchTree()
-tree.insert(5)
-tree.insert(3)
+tree.insert(6)
 tree.insert(4)
+tree.insert(2)
 tree.insert(8)
+tree.insert(5)
+tree.insert(7)
+print(tree.__contains__(8))
+print(tree)
