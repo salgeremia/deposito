b4e0322c526e6f617a288c896b71e2c788486232
salgeremia <salgeremia@gmail.com>
2026-02-04 10:54:16 +0100

    update forme_strane.py solution

--- costrutti_iterativi/forme_strane.py
+++ costrutti_iterativi/forme_strane.py
@@ -8,6 +8,11 @@
 |       |
 |_ _ _ _|
 '''
+n = 4
+print(' _' * n)
+for _ in range(n-1):
+    print('|', ' ' * (2*n-1), '|', sep='')
+print('|', '_ ' * (n-1), '_|', sep='')
 
 # task 2: stampa a video un rettangolo di dimensione nxm
 '''
@@ -16,6 +21,11 @@
 |       |
 |_ _ _ _|
 '''
+m = 3
+print(' _' * n)
+for _ in range(m-1):
+    print('|', ' ' * (2*n-1), '|', sep='')
+print('|', '_ ' * (n-1), '_|', sep='')
 
 # task 3: stampa a video un triangolo isoscele di base n
 ''' 
